7ffc53d645d463515451c163df578c035b26d67f
fumiaki yanagisawa <wakoshi10@gmail.com>
2020-09-23 17:06:32 +0900

    リファクタリング実施

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,56 +1,93 @@
+# ドリンクの料金を定義
+class DrinkPrice
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.cola
+    self.new(:cola,120)
+  end
+
+  def self.redbull
+    self.new(:redbull,200)
+  end
+
+  def self.water
+    self.new(:water,100)
+  end
+end
+
 # 自動販売機の処理
 class VendingMachine
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   MONEY = [10, 50, 100, 500, 1000].freeze
+
+  attr_reader :total, :sales_money, :drink_box
+
   #初期値
   def initialize
-    @slot_money = 0
+    @total = 0
     @sales_money = 0
-    @drink_box = []
-    @drink_box << {:name => "コーラ", :price => 120, :stock => 5}
+    @drinklist = {}
+    5.times { stock(DrinkPrice.cola) }
   end
+
   # 硬貨投入処理
   def slot_money(money)
     return money unless MONEY.include?(money)
-    @slot_money += money
+    @total += money
   end
+
   # 投入金額の総計を取得できる。
   def current_slot_money
-    @slot_money
+    @total
   end
+
   # 払い戻し処理
   def return_money
-    temp_slot_money = @slot_money
-    @slot_money = 0
+    temp_slot_money = @total
+    @total = 0
     temp_slot_money
   end
+
   #売上金の取得
   def current_sales_money
     @sales_money
   end
-  #自動販売機のリスト
-  def drink_list
-    @drink_box.each_with_index do |value, index|
-      status = "購入可能"
-      status = "金額不足" if value[:price] > @slot_money
-      status = "売切" if value[:stock] < 1
-      @drink_box[index][:status] = status
-    end
-  end
+
   # 購入処理
-  def buy(drink_number)
-    if @drink_box[drink_number][:stock] >= 1 && @drink_box[drink_number][:price] <= @slot_money
-      @drink_box[drink_number][:stock] = @drink_box[drink_number][:stock] - 1
-      @sales_money += @drink_box[drink_number][:price]
-      @slot_money -= @drink_box[drink_number][:price]
+  def putout(drink_name)
+    if purchasable?(drink_name)
+      @drinklist[drink_name][:stock] = @drinklist[drink_name][:stock] - 1
+      @sales_money += @drinklist[drink_name][:price]
+      @total -= @drinklist[drink_name][:price]
+      [drink_name, @total]
     end
   end
-  # 既存のドリンクの在庫補充
-  def restock(drink_number, add_stock)
-    @drink_box[drink_number][:stock] = @drink_box[drink_number][:stock] + add_stock
+
+  #購入可能なドリンクリスト表示
+  def purchasable_drink_names
+    @purchasable_drink = []
+    @drinklist.each do |drinks, value|
+      if  value[:price] <= @total &&  value[:stock] >= 1
+        @purchasable_drink << drinks
+      end
+    end
+    @purchasable_drink
   end
-  # 新規のドリンクの在庫補充
-  def new_stock(add_name, add_price, add_stock)
-    @drink_box << {:name => add_name, :price => add_price, :stock => add_stock}
+
+  #購入可能か判定
+  def purchasable?(drink_name)
+    purchasable_drink_names.include? drink_name
   end
+
+  # ドリンクの在庫補充
+  def stock(drink)
+    @drinklist[drink.name] = { price: drink.price, stock: 0 } unless @drinklist.has_key? drink.name
+    @drinklist[drink.name][:stock] += 1
+  end
+
 end
